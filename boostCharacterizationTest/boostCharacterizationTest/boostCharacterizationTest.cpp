30854f0faca609838b39f16592f7b4ca2cb1f88a
Kenichi Nakamura <nkkn1008@gmail.com>
2014-04-12 02:48:37 +0900

    implement tmain for TDD

--- boostCharacterizationTest/boostCharacterizationTest/boostCharacterizationTest.cpp
+++ boostCharacterizationTest/boostCharacterizationTest/boostCharacterizationTest.cpp
@@ -2,10 +2,16 @@
 //
 
 #include "stdafx.h"
-
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	::testing::InitGoogleTest(&argc, argv);
+	::testing::InitGoogleMock(&argc, argv);
+
+	return RUN_ALL_TESTS();
+
 	return 0;
 }
 
