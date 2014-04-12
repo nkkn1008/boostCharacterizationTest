48b6c589e8a6ae71b2240eef39ed410a2404bb57
Kenichi Nakamura <nkkn1008@gmail.com>
2014-04-13 00:00:08 +0900

    BOOST_FOREACHのテストを新規追加した。 各要素を書き換える処理のテスト。

--- boostCharacterizationTest/boostCharacterizationTest/test_BOOST_FOREACH.cpp
+++ boostCharacterizationTest/boostCharacterizationTest/test_BOOST_FOREACH.cpp
@@ -14,4 +14,18 @@ namespace{
 		}
 		ASSERT_THAT(actual.str(), StrEq("3,1,4,"));
 	}
+
+	TEST(test_BOOST_FOREACH, rewrite_each_element){
+		int ar[] = { 3, 1, 4 };
+		std::stringstream actual;
+
+		BOOST_FOREACH(int& x, ar){
+			++x;
+		}
+
+		BOOST_FOREACH(int x, ar){
+			actual << x << ",";
+		}
+		ASSERT_THAT(actual.str(), StrEq("4,2,5,"));
+	}
 }
