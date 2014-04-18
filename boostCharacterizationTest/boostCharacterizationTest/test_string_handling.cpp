0f00332047ce0b6c2b6afa544371c7f6b9cacc17
Kenichi Nakamura <nkkn1008@gmail.com>
2014-04-18 23:44:54 +0900

    boost::splitのテストを追加した。

--- boostCharacterizationTest/boostCharacterizationTest/test_string_handling.cpp
+++ boostCharacterizationTest/boostCharacterizationTest/test_string_handling.cpp
@@ -5,7 +5,12 @@
 #include <gtest/gtest.h>
 
 #include <boost/algorithm/string/trim.hpp>
+#include <boost/algorithm/string/classification.hpp>
+#include <boost/algorithm/string/split.hpp>
+#include <boost/range/algorithm/for_each.hpp>
+#include <boost/assign/list_of.hpp>
 
+using ::testing::Eq;
 using ::testing::StrEq;
 
 namespace{
@@ -14,6 +19,26 @@ namespace{
 		std::string actual = boost::trim_copy(s);
 
 		ASSERT_THAT(actual, StrEq("abc"));
+	}
+
+	TEST(test_string_handling, split_using_comma){
+		const std::string s = "abc,123,xyz";
+		std::vector<std::string> expected = boost::assign::list_of("abc")("123")("xyz");
+
+		std::vector<std::string> actual;
+		boost::algorithm::split(actual, s, boost::is_any_of(","));	
+		
+		ASSERT_THAT(actual, Eq(expected));		
+	}
+
+	TEST(test_string_handling, split_using_slash_or_space){
+		const std::string s = "a/b/c d";
+		std::vector<std::string> expected = boost::assign::list_of("a")("b")("c")("d");
+
+		std::vector<std::string> actual;
+		boost::algorithm::split(actual, s, boost::is_any_of("/ "));
+
+		ASSERT_THAT(actual, Eq(expected));
 	}
 
 }
