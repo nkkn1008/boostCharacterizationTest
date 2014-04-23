eb04803545030fda6b634057e500d050d556b60a
Kenichi Nakamura <nkkn1008@gmail.com>
2014-04-24 07:24:16 +0900

    関数オブジェクトのテストを追加。

--- boostCharacterizationTest/boostCharacterizationTest/test_Function_Pointer.cpp
+++ boostCharacterizationTest/boostCharacterizationTest/test_Function_Pointer.cpp
@@ -19,4 +19,17 @@ namespace{
 		const int actual = f(2, 3);
 		ASSERT_THAT(actual, Eq(5));
 	}
+
+	struct add2 {
+		typedef int result_type;
+		int operator()(int a, int b) const{
+			return a + b;
+		}
+	};
+
+	TEST(test_function_pointer, store_function_object){
+		boost::function<int(int, int)> f = add2();
+		const int actual = f(2, 3);
+		ASSERT_THAT(actual, Eq(5));
+	}
 }
