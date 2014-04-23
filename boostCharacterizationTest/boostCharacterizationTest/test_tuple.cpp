2b35da938df0553d6c383cf71391e780e5db023c
Kenichi Nakamura <nkkn1008@gmail.com>
2014-04-24 07:18:51 +0900

    tuple実行のためにfusionのを利用した。

--- boostCharacterizationTest/boostCharacterizationTest/test_tuple.cpp
+++ boostCharacterizationTest/boostCharacterizationTest/test_tuple.cpp
@@ -6,18 +6,19 @@
 
 #include <boost/fusion/include/vector.hpp>
 #include <boost/fusion/include/at_c.hpp>
+#include <boost/fusion/include/for_each.hpp>
 
 using ::testing::Eq;
 using ::testing::StrEq;
 
+namespace fusion = boost::fusion;
+
 namespace{
 	TEST(test_tuple, at_c_n){
-		namespace fusion = boost::fusion;
 		const fusion::vector<int, char, std::string> v(1, 'a', "Hello");
-		
+
 		ASSERT_THAT(fusion::at_c<0>(v), Eq(1));
 		ASSERT_THAT(fusion::at_c<1>(v), Eq('a'));
 		ASSERT_THAT(fusion::at_c<2>(v), StrEq("Hello"));
-	}
-
+	}	
 }
